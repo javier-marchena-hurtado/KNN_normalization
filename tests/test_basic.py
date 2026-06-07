04d3a137a3ac215810c8681582de82204437c892
Javier Marchena Hurtado <javier.marchena.hurtado@gmail.com>
2026-06-07 15:03:36 +0200

    Renamed knn_normalize_protein() to knn_normalize() in tests

--- tests/test_basic.py
+++ tests/test_basic.py
@@ -2,17 +2,17 @@ import numpy as np
 from anndata import AnnData
 from mudata import MuData
 
-import knn_normalization
+import knn_normalization as knn
 
 
 def test_package_has_version():
-    assert knn_normalization.__version__ is not None
+    assert knn.__version__ is not None
 
 
 def test_retrieve_neighbors(adata):
     """This test tests whether the function retrieve_neighbors works as intended."""
 
-    neighbors = knn_normalization.pp.retrieve_neighbors(adata.obsp["connectivities"])
+    neighbors = knn.pp.retrieve_neighbors(adata.obsp["connectivities"])
     assert isinstance(neighbors, dict)
     assert len(neighbors) == adata.n_obs
 
@@ -21,37 +21,31 @@ def test_normalize_with_neighbors(adata):
     """This test tests whether the function _normalize_with_neighbors works as intended."""
     original_data = adata.X.copy()
     neighbors = adata.obsp["connectivities"]
-    knn_normalization.tl._normalize_with_neighbors(adata, neighbors, log_transform=False, inplace=True)
+    knn.tl._normalize_with_neighbors(adata, neighbors, log_transform=False, inplace=True)
     assert not np.allclose(adata.X, original_data)  # Data should be changed
 
 
 def test_calculate_neighbors_from_protein(adata):
     """This test tests whether the function calculate_neighbors_from_protein works as intended."""
-    connectivities = knn_normalization.pp.calculate_neighbors_from_protein(adata, n_neighbors=3, log_transform=True)
-    neighbors = knn_normalization.pp.retrieve_neighbors(connectivities)
+    connectivities = knn.pp.calculate_neighbors_from_protein(adata, n_neighbors=3, log_transform=True)
+    neighbors = knn.pp.retrieve_neighbors(connectivities)
     assert isinstance(neighbors, dict)
     assert len(neighbors) > 0
 
 
-def test_knn_normalize_protein_anndata(adata):
-    """This test tests whether the function knn_normalize_protein works as intended, using an AnnData object as input."""
-    knn_normalization.tl.knn_normalize_protein(
-        adata, calculate_neighbors_from="prot", n_neighbors=3, inplace=True, log_transform=True
-    )
+def test_knn_normalize_anndata(adata):
+    """This test tests whether the function knn_normalize works as intended, using an AnnData object as input."""
+    knn.tl.knn_normalize(adata, calculate_neighbors_from="prot", n_neighbors=3, inplace=True, log_transform=True)
     assert isinstance(adata, AnnData)
 
 
-def test_knn_normalize_protein_mudata_rna_neighbors(mdata):
-    """This test tests whether the function knn_normalize_protein works as intended, using an MuData object as input and with RNA neighbors."""
-    result = knn_normalization.tl.knn_normalize_protein(
-        mdata, calculate_neighbors_from="rna", n_neighbors=3, inplace=False
-    )
+def test_knn_normalize_mudata_rna_neighbors(mdata):
+    """This test tests whether the function knn_normalize works as intended, using an MuData object as input and with RNA neighbors."""
+    result = knn.tl.knn_normalize(mdata, calculate_neighbors_from="rna", n_neighbors=3, inplace=False)
     assert isinstance(result, MuData)
 
 
-def test_knn_normalize_protein_mudata_protein_neighbors(mdata):
-    """This test tests whether the function knn_normalize_protein works as intended, using an MuData object as input and with protein neighbors."""
-    knn_normalization.tl.knn_normalize_protein(
-        mdata, calculate_neighbors_from="prot", n_neighbors=3, inplace=True, log_transform=True
-    )
+def test_knn_normalize_mudata_protein_neighbors(mdata):
+    """This test tests whether the function knn_normalize works as intended, using an MuData object as input and with protein neighbors."""
+    knn.tl.knn_normalize(mdata, calculate_neighbors_from="prot", n_neighbors=3, inplace=True, log_transform=True)
     assert isinstance(mdata, MuData)
